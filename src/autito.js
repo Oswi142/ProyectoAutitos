e59a4b4abe619b6aa45e95efbd7286b95436c775
oswaldoroman <edwin.roman@ucb.edu.bo>
2023-09-04 16:27:21 -0400

    Hacemos refactoring y la última prueba

--- src/autito.js
+++ src/autito.js
@@ -6,34 +6,44 @@ class Autito{
         this.direc=direc;
     }
 
-    generarTablero(filas,columnas)
+    generarTablero(filas, columnas)
     {
-        this.filas = filas;
-        this.columnas=columnas;
-        this.matriz = new Array(filas);
-        for (let i = 0; i < filas+1; i++) 
-        {
-            this.matriz[i] = new Array(columnas);
-        }
+      this.filas = filas;
+      this.columnas=columnas;
+      this.matriz = new Array(filas);
+      for (let i = 0; i < filas+1; i++) 
+      {
+          this.matriz[i] = new Array(columnas);
+      }
     }
     
-    asignarValores(cadena) 
+    asignarValores(comando)
     {
-        try {
-          const valores = cadena.split(',');
-          this.x = +valores[0];
-          this.y = +valores[1];
-          this.direccion = valores[2];
-        } catch {
-          alert("Valores erroneos");
+        var cadena = comando.toString().split(',');
+        if (cadena.length !== 3)
+        {
+            return "Valores erroneos";    
         }
-      }
+        else
+        {
+            this.x = +cadena[0];
+            this.y = +cadena[1];
+            this.direc = cadena[2];
+        }
+    }
 
     definirPosIni(comando)
     {
-        let cadena = comando.toString().split(',');
-        this.asignarValores(cadena);
-        return comando;
+        let res=this.asignarValores(comando);
+        if(res === undefined)
+        {
+            return comando;
+        }
+        else
+        {
+            return res;
+        }
+
     }
 }
 
--- src/autito.test.js
+++ src/autito.test.js
@@ -1,28 +1,33 @@
-import Autito from './autito';
+import Autito from './Autito';
 
 describe('Autito', () => {
-  let autito;
+  let autito = new Autito(0,0,'N');
 
-  beforeEach(() => {
-    autito = new Autito(0,0,'N');
+  it('debería generar un tablero correctamente', () => {
+    autito.generarTablero(5,5);
+    expect(autito.matriz.length).toBe(6);
+    expect(autito.matriz[0].length).toBe(5);
   });
 
-  it('debería crear un objeto Autito con las propiedades adecuadas', () => {
-    expect(autito.x).toBe(0);
-    expect(autito.y).toBe(0);
+  it('debería asignar valores de posición correctamente', () => {
+    autito.asignarValores("2,3,N");
+    expect(autito.x).toBe(2);
+    expect(autito.y).toBe(3);
     expect(autito.direc).toBe('N');
   });
 
-  it('debería generar una matriz de tamaño correcto al llamar generarTablero', () => {
-    autito.generarTablero(5,5);
-    expect(autito.filas).toBe(5);
-    expect(autito.columnas).toBe(5);
+  it('debería definir la posición inicial correctamente', () => {
+    const comando = "4,2,E";
+    const resultado = autito.definirPosIni(comando);
+    expect(resultado).toBe(comando);
+    expect(autito.x).toBe(4);
+    expect(autito.y).toBe(2);
+    expect(autito.direc).toBe("E");
   });
 
-  it('debería asignar valores correctamente al llamar asignarValores', () => {
-    autito.asignarValores('3,4,N');
-    expect(autito.x).toBe(3);
-    expect(autito.y).toBe(4);
-    expect(autito.direccion).toBe('N');
+  it('debería retornar error', () => {
+    const comando = "4,2";
+    const resultado = autito.definirPosIni(comando);
+    expect(resultado).toEqual("Valores erroneos");
   });
 });
