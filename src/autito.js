96239e8d3a9e367517adecdbaa28648bac50d4b4
oswaldoroman <edwin.roman@ucb.edu.bo>
2023-09-03 23:34:07 -0400

    Creamos la función de generar tablero

--- src/autito.js
+++ src/autito.js
@@ -5,4 +5,15 @@ class Autito{
         this.y=y;
         this.direc=direc;
     }
+
+    generarTablero(filas,columnas)
+    {
+        this.filas = filas;
+        this.columnas=columnas;
+        this.matriz = new Array(filas);
+        for (let i = 0; i < filas+1; i++) 
+        {
+            this.matriz[i] = new Array(columnas);
+        }
+    }
 }
